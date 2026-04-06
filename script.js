78beb1cdb79aedd5c843e376b1a7a9c66875c847
scribencode <deyeye99@gmail.com>
2026-04-06 17:58:53 +0100

    add drum sound effect logic

--- script.js
+++ script.js
@@ -10,67 +10,56 @@ let snareEl = document.getElementById("snare-el");
 let tomEl = document.getElementById("tom-el");
 let tinkEl = document.getElementById("tink-el");
 
+// function to play sound and add animation
+
+function playSound(element, path) {
+  let audioInsance = new Audio(path);
+  //   Reset sound to start if clicked quickly
+  audioInsance.currentTime = 0;
+  audioInsance.play();
+  //   add animation effect with playing class
+  element.classList.add("playing");
+
+  // Remove it after 100ms so the "flash" ends
+  setTimeout(() => {
+    element.classList.remove("playing");
+  }, 100);
+}
+
 // listen to all button clicks
 
-clapEl.addEventListener("click", () => {
-  let audioInsance = new Audio("audios/clap-effect.mp3");
-  //   Reset sound to start if clicked quickly
-  audioInsance.currentTime = 0;
-  audioInsance.play();
-});
+clapEl.addEventListener("click", () =>
+  playSound(clapEl, "audios/clap-effect.mp3"),
+);
 
-hihatEl.addEventListener("click", () => {
-  let audioInsance = new Audio("audios/HiHat-effect.mp3");
-  //   Reset sound to start if clicked quickly
-  audioInsance.currentTime = 0;
-  audioInsance.play();
-});
+hihatEl.addEventListener("click", () =>
+  playSound(hihatEl, "audios/HiHat-effect.mp3"),
+);
 
-kickEl.addEventListener("click", () => {
-  let audioInsance = new Audio("audios/Kick-effect.mp3");
-  //   Reset sound to start if clicked quickly
-  audioInsance.currentTime = 0;
-  audioInsance.play();
-});
+kickEl.addEventListener("click", () =>
+  playSound(kickEl, "audios/Kick-effect.mp3"),
+);
 
-openhatEl.addEventListener("click", () => {
-  let audioInsance = new Audio("audios/OpenHat.mp3");
-  //   Reset sound to start if clicked quickly
-  audioInsance.currentTime = 0;
-  audioInsance.play();
-});
+openhatEl.addEventListener("click", () =>
+  playSound(openhatEl, "audios/OpenHat.mp3"),
+);
 
-boomEl.addEventListener("click", () => {
-  let audioInsance = new Audio("audios/boom-effect.mp3");
-  //   Reset sound to start if clicked quickly
-  audioInsance.currentTime = 0;
-  audioInsance.play();
-});
+boomEl.addEventListener("click", () =>
+  playSound(boomEl, "audios/boom-effect.mp3"),
+);
 
-rideEl.addEventListener("click", () => {
-  let audioInsance = new Audio("audios/Ride-effect.mp3");
-  //   Reset sound to start if clicked quickly
-  audioInsance.currentTime = 0;
-  audioInsance.play();
-});
+rideEl.addEventListener("click", () =>
+  playSound(rideEl, "audios/Ride-effect.mp3"),
+);
 
-snareEl.addEventListener("click", () => {
-  let audioInsance = new Audio("audios/Snare-effect.mp3");
-  //   Reset sound to start if clicked quickly
-  audioInsance.currentTime = 0;
-  audioInsance.play();
-});
+snareEl.addEventListener("click", () =>
+  playSound(snareEl, "audios/Snare-effect.mp3"),
+);
 
-tomEl.addEventListener("click", () => {
-  let audioInsance = new Audio("audios/Tom-effect.mp3");
-  //   Reset sound to start if clicked quickly
-  audioInsance.currentTime = 0;
-  audioInsance.play();
-});
+tomEl.addEventListener("click", () =>
+  playSound(tomEl, "audios/Tom-effect.mp3"),
+);
 
-tinkEl.addEventListener("click", () => {
-  let audioInsance = new Audio("audios/Tink-effect.mp3");
-  //   Reset sound to start if clicked quickly
-  audioInsance.currentTime = 0;
-  audioInsance.play();
-});
+tinkEl.addEventListener("click", () =>
+  playSound(tinkEl, "audios/Tink-effect.mp3"),
+);
